3f661b84e5cbf8d6cd47a08b867529a8dcc89c68
lam565 <el.aziz565@gmail.com>
2024-01-30 08:27:20 +0700

    Halaman 2 mulai

--- baru_test/pdf_pelaporan.php
+++ baru_test/pdf_pelaporan.php
@@ -24,7 +24,7 @@ function labelForm($pdf, $label)
 
 $pdf = new FPDF('P', 'mm', 'A4'); //A4 = 210 mm x 297mm
 $pdf->Open();
-$pdf->SetMargins(10,8);
+$pdf->SetMargins(10, 8);
 $pdf->AddPage();
 
 $t1 = 3.5;
@@ -85,25 +85,25 @@ $pdf->SetFont('Times', 'B', $fz2);
 $pdf->Cell(0, $t2, 'DATA PELAPOR', 'LTR', 1, 'L');
 
 $pdf->SetFont('Times', '', $fz1);
-labelForm($pdf,'Nama');
-isiKotak($pdf,'',30);
-$pdf->Cell(0,$t1,'','R',1);
+labelForm($pdf, 'Nama');
+isiKotak($pdf, '', 30);
+$pdf->Cell(0, $t1, '', 'R', 1);
 
-labelForm($pdf,'NIK');
-isiKotak($pdf,'',16);
-$pdf->Cell(0,$t1,'','R',1);
+labelForm($pdf, 'NIK');
+isiKotak($pdf, '', 16);
+$pdf->Cell(0, $t1, '', 'R', 1);
 
-labelForm($pdf,'Nomor Dokumen Perjalanan *');
-isiKotak($pdf,'',16);
-$pdf->Cell(0,$t1,'','R',1);
+labelForm($pdf, 'Nomor Dokumen Perjalanan *');
+isiKotak($pdf, '', 16);
+$pdf->Cell(0, $t1, '', 'R', 1);
 
-labelForm($pdf,'Nomor Kartu Keluarga');
-isiKotak($pdf,'',16);
-$pdf->Cell(0,$t1,'','R',1);
+labelForm($pdf, 'Nomor Kartu Keluarga');
+isiKotak($pdf, '', 16);
+$pdf->Cell(0, $t1, '', 'R', 1);
 
-labelForm($pdf,'Kewarganegaraan');
-isiKotak($pdf,'',30);
-$pdf->Cell(0,$t1,'','R',1);
+labelForm($pdf, 'Kewarganegaraan');
+isiKotak($pdf, '', 30);
+$pdf->Cell(0, $t1, '', 'R', 1);
 
 $pdf->Cell(0, $t1, '', 'LBR', 1, 'L');
 $pdf->Ln(1);
@@ -113,25 +113,25 @@ $pdf->SetFont('Times', 'B', $fz2);
 $pdf->Cell(0, $t2, 'DATA SUBJEK AKTA KESATU', 'LTR', 1, 'L');
 
 $pdf->SetFont('Times', '', $fz1);
-labelForm($pdf,'Nama');
-isiKotak($pdf,'',30);
-$pdf->Cell(0,$t1,'','R',1);
+labelForm($pdf, 'Nama');
+isiKotak($pdf, '', 30);
+$pdf->Cell(0, $t1, '', 'R', 1);
 
-labelForm($pdf,'NIK');
-isiKotak($pdf,'',16);
-$pdf->Cell(0,$t1,'','R',1);
+labelForm($pdf, 'NIK');
+isiKotak($pdf, '', 16);
+$pdf->Cell(0, $t1, '', 'R', 1);
 
-labelForm($pdf,'Nomor Dokumen Perjalanan *');
-isiKotak($pdf,'',16);
-$pdf->Cell(0,$t1,'','R',1);
+labelForm($pdf, 'Nomor Dokumen Perjalanan *');
+isiKotak($pdf, '', 16);
+$pdf->Cell(0, $t1, '', 'R', 1);
 
-labelForm($pdf,'Nomor Kartu Keluarga');
-isiKotak($pdf,'',16);
-$pdf->Cell(0,$t1,'','R',1);
+labelForm($pdf, 'Nomor Kartu Keluarga');
+isiKotak($pdf, '', 16);
+$pdf->Cell(0, $t1, '', 'R', 1);
 
-labelForm($pdf,'Kewarganegaraan');
-isiKotak($pdf,'',30);
-$pdf->Cell(0,$t1,'','R',1);
+labelForm($pdf, 'Kewarganegaraan');
+isiKotak($pdf, '', 30);
+$pdf->Cell(0, $t1, '', 'R', 1);
 
 $pdf->Cell(0, $t1, '', 'LBR', 1, 'L');
 $pdf->Ln(1);
@@ -141,25 +141,25 @@ $pdf->SetFont('Times', 'B', $fz2);
 $pdf->Cell(0, $t2, 'DATA SUBJEK AKTA KEDUA (JIKA ADA)', 'LTR', 1, 'L');
 
 $pdf->SetFont('Times', '', $fz1);
-labelForm($pdf,'Nama');
-isiKotak($pdf,'',30);
-$pdf->Cell(0,$t1,'','R',1);
+labelForm($pdf, 'Nama');
+isiKotak($pdf, '', 30);
+$pdf->Cell(0, $t1, '', 'R', 1);
 
-labelForm($pdf,'NIK');
-isiKotak($pdf,'',16);
-$pdf->Cell(0,$t1,'','R',1);
+labelForm($pdf, 'NIK');
+isiKotak($pdf, '', 16);
+$pdf->Cell(0, $t1, '', 'R', 1);
 
-labelForm($pdf,'Nomor Dokumen Perjalanan *');
-isiKotak($pdf,'',16);
-$pdf->Cell(0,$t1,'','R',1);
+labelForm($pdf, 'Nomor Dokumen Perjalanan *');
+isiKotak($pdf, '', 16);
+$pdf->Cell(0, $t1, '', 'R', 1);
 
-labelForm($pdf,'Nomor Kartu Keluarga');
-isiKotak($pdf,'',16);
-$pdf->Cell(0,$t1,'','R',1);
+labelForm($pdf, 'Nomor Kartu Keluarga');
+isiKotak($pdf, '', 16);
+$pdf->Cell(0, $t1, '', 'R', 1);
 
-labelForm($pdf,'Kewarganegaraan');
-isiKotak($pdf,'',30);
-$pdf->Cell(0,$t1,'','R',1);
+labelForm($pdf, 'Kewarganegaraan');
+isiKotak($pdf, '', 30);
+$pdf->Cell(0, $t1, '', 'R', 1);
 
 $pdf->Cell(0, $t1, '', 'LBR', 1, 'L');
 $pdf->Ln(1);
@@ -169,41 +169,41 @@ $pdf->SetFont('Times', 'B', $fz2);
 $pdf->Cell(0, $t2, 'DATA SAKSI I', 'LTR', 1, 'L');
 
 $pdf->SetFont('Times', '', $fz1);
-labelForm($pdf,'Nama');
-isiKotak($pdf,'',30);
-$pdf->Cell(0,$t1,'','R',1);
+labelForm($pdf, 'Nama');
+isiKotak($pdf, '', 30);
+$pdf->Cell(0, $t1, '', 'R', 1);
 
-labelForm($pdf,'NIK');
-isiKotak($pdf,'',16);
-$pdf->Cell(0,$t1,'','R',1);
+labelForm($pdf, 'NIK');
+isiKotak($pdf, '', 16);
+$pdf->Cell(0, $t1, '', 'R', 1);
 
-labelForm($pdf,'Nomor Kartu Keluarga');
-isiKotak($pdf,'',16);
-$pdf->Cell(0,$t1,'','R',1);
+labelForm($pdf, 'Nomor Kartu Keluarga');
+isiKotak($pdf, '', 16);
+$pdf->Cell(0, $t1, '', 'R', 1);
 
-labelForm($pdf,'Kewarganegaraan');
-isiKotak($pdf,'',30);
-$pdf->Cell(0,$t1,'','R',1);
+labelForm($pdf, 'Kewarganegaraan');
+isiKotak($pdf, '', 30);
+$pdf->Cell(0, $t1, '', 'R', 1);
 
 $pdf->SetFont('Times', 'B', $fz2);
 $pdf->Cell(0, $t2, 'DATA SAKSI II', 'LR', 1, 'L');
 
 $pdf->SetFont('Times', '', $fz1);
-labelForm($pdf,'Nama');
-isiKotak($pdf,'',30);
-$pdf->Cell(0,$t1,'','R',1);
+labelForm($pdf, 'Nama');
+isiKotak($pdf, '', 30);
+$pdf->Cell(0, $t1, '', 'R', 1);
 
-labelForm($pdf,'NIK');
-isiKotak($pdf,'',16);
-$pdf->Cell(0,$t1,'','R',1);
+labelForm($pdf, 'NIK');
+isiKotak($pdf, '', 16);
+$pdf->Cell(0, $t1, '', 'R', 1);
 
-labelForm($pdf,'Nomor Kartu Keluarga');
-isiKotak($pdf,'',16);
-$pdf->Cell(0,$t1,'','R',1);
+labelForm($pdf, 'Nomor Kartu Keluarga');
+isiKotak($pdf, '', 16);
+$pdf->Cell(0, $t1, '', 'R', 1);
 
-labelForm($pdf,'Kewarganegaraan');
-isiKotak($pdf,'',30);
-$pdf->Cell(0,$t1,'','R',1);
+labelForm($pdf, 'Kewarganegaraan');
+isiKotak($pdf, '', 30);
+$pdf->Cell(0, $t1, '', 'R', 1);
 
 $pdf->Cell(0, $t1, '', 'LBR', 1, 'L');
 $pdf->Ln(1);
@@ -213,55 +213,55 @@ $pdf->SetFont('Times', 'BIU', $fz2);
 $pdf->Cell(0, $t2, 'DATA ORANG TUA** (Hanya diisi untuk keperluan pencatatan kelahiran, lahir mati dan kematian)', 'LTR', 1, 'L');
 
 $pdf->SetFont('Times', '', $fz1);
-labelForm($pdf,'Nama Ayah');
-isiKotak($pdf,'',30);
-$pdf->Cell(0,$t1,'','R',1);
+labelForm($pdf, 'Nama Ayah');
+isiKotak($pdf, '', 30);
+$pdf->Cell(0, $t1, '', 'R', 1);
 
-labelForm($pdf,'NIK Ayah');
-isiKotak($pdf,'',16);
-$pdf->Cell(0,$t1,'','R',1);
+labelForm($pdf, 'NIK Ayah');
+isiKotak($pdf, '', 16);
+$pdf->Cell(0, $t1, '', 'R', 1);
 
-labelForm($pdf,'Tempat Lahir Ayah');
-isiKotak($pdf,'',16);
-$pdf->Cell(0,$t1,'','R',1);
+labelForm($pdf, 'Tempat Lahir Ayah');
+isiKotak($pdf, '', 16);
+$pdf->Cell(0, $t1, '', 'R', 1);
 
-labelForm($pdf,'Tanggal Lahir Ayah');
-$pdf->Cell(4*$t1,$t1,'Tgl. ',0,0);
-isiKotak($pdf,'',2);
-$pdf->Cell(4*$t1,$t1,'Bln. ',0,0);
-isiKotak($pdf,'',2);
-$pdf->Cell(4*$t1,$t1,'Thn. ',0,0);
-isiKotak($pdf,'',4);
-$pdf->Cell(0,$t1,'','R',1);
+labelForm($pdf, 'Tanggal Lahir Ayah');
+$pdf->Cell(4 * $t1, $t1, 'Tgl. ', 0, 0);
+isiKotak($pdf, '', 2);
+$pdf->Cell(4 * $t1, $t1, 'Bln. ', 0, 0);
+isiKotak($pdf, '', 2);
+$pdf->Cell(4 * $t1, $t1, 'Thn. ', 0, 0);
+isiKotak($pdf, '', 4);
+$pdf->Cell(0, $t1, '', 'R', 1);
 
-labelForm($pdf,'Kewarganegaraan');
-isiKotak($pdf,'',30);
-$pdf->Cell(0,$t1,'','R',1);
+labelForm($pdf, 'Kewarganegaraan');
+isiKotak($pdf, '', 30);
+$pdf->Cell(0, $t1, '', 'R', 1);
 
-labelForm($pdf,'Nama Ibu');
-isiKotak($pdf,'',30);
-$pdf->Cell(0,$t1,'','R',1);
+labelForm($pdf, 'Nama Ibu');
+isiKotak($pdf, '', 30);
+$pdf->Cell(0, $t1, '', 'R', 1);
 
-labelForm($pdf,'NIK Ibu');
-isiKotak($pdf,'',16);
-$pdf->Cell(0,$t1,'','R',1);
+labelForm($pdf, 'NIK Ibu');
+isiKotak($pdf, '', 16);
+$pdf->Cell(0, $t1, '', 'R', 1);
 
-labelForm($pdf,'Tempat Lahir Ibu');
-isiKotak($pdf,'',16);
-$pdf->Cell(0,$t1,'','R',1);
+labelForm($pdf, 'Tempat Lahir Ibu');
+isiKotak($pdf, '', 16);
+$pdf->Cell(0, $t1, '', 'R', 1);
 
-labelForm($pdf,'Tanggal Lahir Ibu');
-$pdf->Cell(4*$t1,$t1,'Tgl. ',0,0);
-isiKotak($pdf,'',2);
-$pdf->Cell(4*$t1,$t1,'Bln. ',0,0);
-isiKotak($pdf,'',2);
-$pdf->Cell(4*$t1,$t1,'Thn. ',0,0);
-isiKotak($pdf,'',4);
-$pdf->Cell(0,$t1,'','R',1);
+labelForm($pdf, 'Tanggal Lahir Ibu');
+$pdf->Cell(4 * $t1, $t1, 'Tgl. ', 0, 0);
+isiKotak($pdf, '', 2);
+$pdf->Cell(4 * $t1, $t1, 'Bln. ', 0, 0);
+isiKotak($pdf, '', 2);
+$pdf->Cell(4 * $t1, $t1, 'Thn. ', 0, 0);
+isiKotak($pdf, '', 4);
+$pdf->Cell(0, $t1, '', 'R', 1);
 
-labelForm($pdf,'Kewarganegaraan');
-isiKotak($pdf,'',30);
-$pdf->Cell(0,$t1,'','R',1);
+labelForm($pdf, 'Kewarganegaraan');
+isiKotak($pdf, '', 30);
+$pdf->Cell(0, $t1, '', 'R', 1);
 
 $pdf->Cell(0, $t1, '', 'LBR', 1, 'L');
 $pdf->Ln(1);
@@ -271,86 +271,86 @@ $pdf->SetFont('Times', 'B', $fz2);
 $pdf->Cell(0, $t2, 'DATA ANAK', 'LTR', 1, 'L');
 
 $pdf->SetFont('Times', '', $fz1);
-labelForm($pdf,'1. Nama');
+labelForm($pdf, '1. Nama');
 //isikan nama disini
-$pdf->Cell(0,$t1,'','R',1);
+$pdf->Cell(0, $t1, '', 'R', 1);
 
-labelForm($pdf,'2. Jenis Kelamin');
-isiKotak($pdf,'',1);
-$pdf->Cell(10*$t1,$t1,'1. Laki-laki ',0,0,'L');
-isiKotak($pdf,'',1);
-$pdf->Cell(10*$t1,$t1,'2. Perempuan ',0,0,'L');
-$pdf->Cell(0,$t1,'','R',1);
+labelForm($pdf, '2. Jenis Kelamin');
+isiKotak($pdf, '', 1);
+$pdf->Cell(10 * $t1, $t1, '1. Laki-laki ', 0, 0, 'L');
+isiKotak($pdf, '', 1);
+$pdf->Cell(10 * $t1, $t1, '2. Perempuan ', 0, 0, 'L');
+$pdf->Cell(0, $t1, '', 'R', 1);
 
-labelForm($pdf,'3. Tempat dilahirkan');
-isiKotak($pdf,'',1);
-$pdf->Cell(5*$t1,$t1,'1. RS/RB ',0,0,'L');
-isiKotak($pdf,'',1);
-$pdf->Cell(7*$t1,$t1,'2. Puskesmas ',0,0,'L');
-isiKotak($pdf,'',1);
-$pdf->Cell(8*$t1,$t1,'3. Polides ',0,0,'L');
-isiKotak($pdf,'',1);
-$pdf->Cell(7*$t1,$t1,'4. Rumah ',0,0,'L');
-isiKotak($pdf,'',1);
-$pdf->Cell(7*$t1,$t1,'5. Lainnya ',0,0,'L');
-$pdf->Cell(0,$t1,'','R',1);
+labelForm($pdf, '3. Tempat dilahirkan');
+isiKotak($pdf, '', 1);
+$pdf->Cell(5 * $t1, $t1, '1. RS/RB ', 0, 0, 'L');
+isiKotak($pdf, '', 1);
+$pdf->Cell(7 * $t1, $t1, '2. Puskesmas ', 0, 0, 'L');
+isiKotak($pdf, '', 1);
+$pdf->Cell(8 * $t1, $t1, '3. Polides ', 0, 0, 'L');
+isiKotak($pdf, '', 1);
+$pdf->Cell(7 * $t1, $t1, '4. Rumah ', 0, 0, 'L');
+isiKotak($pdf, '', 1);
+$pdf->Cell(7 * $t1, $t1, '5. Lainnya ', 0, 0, 'L');
+$pdf->Cell(0, $t1, '', 'R', 1);
 
-labelForm($pdf,'4. Tempat Kelahiran');
+labelForm($pdf, '4. Tempat Kelahiran');
 //isikan tempat lahir disini
-$pdf->Cell(0,$t1,'','R',1);
+$pdf->Cell(0, $t1, '', 'R', 1);
 
-labelForm($pdf,'5. Hari dan tanggal lahir');
-$pdf->Cell(5*$t1,$t1,'Hari: ',0,0);
-isiKotak($pdf,'',2);
-$pdf->Cell(4*$t1,$t1,'Tgl: ',0,0);
-isiKotak($pdf,'',2);
-$pdf->Cell(4*$t1,$t1,'Bln: ',0,0);
-isiKotak($pdf,'',2);
-$pdf->Cell(4*$t1,$t1,'Thn: ',0,0);
-isiKotak($pdf,'',4);
-$pdf->Cell(0,$t1,'','R',1);
+labelForm($pdf, '5. Hari dan tanggal lahir');
+$pdf->Cell(5 * $t1, $t1, 'Hari: ', 0, 0);
+isiKotak($pdf, '', 2);
+$pdf->Cell(4 * $t1, $t1, 'Tgl: ', 0, 0);
+isiKotak($pdf, '', 2);
+$pdf->Cell(4 * $t1, $t1, 'Bln: ', 0, 0);
+isiKotak($pdf, '', 2);
+$pdf->Cell(4 * $t1, $t1, 'Thn: ', 0, 0);
+isiKotak($pdf, '', 4);
+$pdf->Cell(0, $t1, '', 'R', 1);
 
-labelForm($pdf,'6. Pukul');
+labelForm($pdf, '6. Pukul');
 //isikan pukul disini
-$pdf->Cell(0,$t1,'','R',1);
+$pdf->Cell(0, $t1, '', 'R', 1);
 
-labelForm($pdf,'7. Jenis Kelahiran');
-isiKotak($pdf,'',1);
-$pdf->Cell(6*$t1,$t1,'1. Tunggal ',0,0,'L');
-isiKotak($pdf,'',1);
-$pdf->Cell(6*$t1,$t1,'2. Kembar 2 ',0,0,'L');
-isiKotak($pdf,'',1);
-$pdf->Cell(6*$t1,$t1,'3. Kembar 3 ',0,0,'L');
-isiKotak($pdf,'',1);
-$pdf->Cell(6*$t1,$t1,'4. Kembar 4 ',0,0,'L');
-isiKotak($pdf,'',1);
-$pdf->Cell(7*$t1,$t1,'5. Lainnya ',0,0,'L');
-$pdf->Cell(0,$t1,'','R',1);
+labelForm($pdf, '7. Jenis Kelahiran');
+isiKotak($pdf, '', 1);
+$pdf->Cell(6 * $t1, $t1, '1. Tunggal ', 0, 0, 'L');
+isiKotak($pdf, '', 1);
+$pdf->Cell(6 * $t1, $t1, '2. Kembar 2 ', 0, 0, 'L');
+isiKotak($pdf, '', 1);
+$pdf->Cell(6 * $t1, $t1, '3. Kembar 3 ', 0, 0, 'L');
+isiKotak($pdf, '', 1);
+$pdf->Cell(6 * $t1, $t1, '4. Kembar 4 ', 0, 0, 'L');
+isiKotak($pdf, '', 1);
+$pdf->Cell(7 * $t1, $t1, '5. Lainnya ', 0, 0, 'L');
+$pdf->Cell(0, $t1, '', 'R', 1);
 
-labelForm($pdf,'8. Kelahiran ke');
+labelForm($pdf, '8. Kelahiran ke');
 //isikan disini
-$pdf->Cell(0,$t1,'','R',1);
+$pdf->Cell(0, $t1, '', 'R', 1);
 
-labelForm($pdf,'9. Penolong Kelahiran');
-isiKotak($pdf,'',1);
-$pdf->Cell(7*$t1,$t1,'1. Dokter ',0,0,'L');
-isiKotak($pdf,'',1);
-$pdf->Cell(10*$t1,$t1,'2. Bidan/Perwat ',0,0,'L');
-isiKotak($pdf,'',1);
-$pdf->Cell(7*$t1,$t1,'3. Dukun ',0,0,'L');
-isiKotak($pdf,'',1);
-$pdf->Cell(7*$t1,$t1,'5. Lainnya ',0,0,'L');
-$pdf->Cell(0,$t1,'','R',1);
+labelForm($pdf, '9. Penolong Kelahiran');
+isiKotak($pdf, '', 1);
+$pdf->Cell(7 * $t1, $t1, '1. Dokter ', 0, 0, 'L');
+isiKotak($pdf, '', 1);
+$pdf->Cell(10 * $t1, $t1, '2. Bidan/Perwat ', 0, 0, 'L');
+isiKotak($pdf, '', 1);
+$pdf->Cell(7 * $t1, $t1, '3. Dukun ', 0, 0, 'L');
+isiKotak($pdf, '', 1);
+$pdf->Cell(7 * $t1, $t1, '5. Lainnya ', 0, 0, 'L');
+$pdf->Cell(0, $t1, '', 'R', 1);
 
-labelForm($pdf,'10. Berat Bayi');
-$pdf->Cell(4*$t1,$t1,'',1,0);
-$pdf->Cell(3*$t1,$t1,' Kg',0,0);
-$pdf->Cell(0,$t1,'','R',1);
+labelForm($pdf, '10. Berat Bayi');
+$pdf->Cell(4 * $t1, $t1, '', 1, 0);
+$pdf->Cell(3 * $t1, $t1, ' Kg', 0, 0);
+$pdf->Cell(0, $t1, '', 'R', 1);
 
-labelForm($pdf,'11. Panjang Bayi');
-$pdf->Cell(4*$t1,$t1,'',1,0);
-$pdf->Cell(3*$t1,$t1,' Cm',0,0);
-$pdf->Cell(0,$t1,'','R',1);
+labelForm($pdf, '11. Panjang Bayi');
+$pdf->Cell(4 * $t1, $t1, '', 1, 0);
+$pdf->Cell(3 * $t1, $t1, ' Cm', 0, 0);
+$pdf->Cell(0, $t1, '', 'R', 1);
 
 $pdf->Cell(0, $t1, '', 'LBR', 1, 'L');
 $pdf->Ln(1);
